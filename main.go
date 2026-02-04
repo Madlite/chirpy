52f92c6cfa4a0ad97c08920ed45a0a3dfa2a17f0
Madlite <mistermadlite@gmail.com>
2026-02-04 12:50:46 +0100

    app and logo serve

--- main.go
+++ main.go
@@ -9,11 +9,22 @@ type Server struct {
     Addr 	string
 }
 
+
 func main() {
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+
 	server.ListenAndServe()
 }
