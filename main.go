582f9a98615e4c8ba6029d7844da38c85394e498
Madlite <mistermadlite@gmail.com>
2026-02-04 10:46:02 +0100

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+
+import(
+	"net/http"
+)
+
+type Server struct {
+    Addr 	string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
